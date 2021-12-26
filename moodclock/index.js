476ccb0f8d08d29ea1ea3ae6ff906cf22594e021
Luke Patterson <luke.k.patterson@gmail.com>
2021-12-26 15:32:21 -0500

    fix height of pointer on moodclock

--- moodclock/index.js
+++ moodclock/index.js
@@ -18,6 +18,19 @@ window.onload = function () {
   }
 
   const arrow = document.querySelector('#pointer');
+  const clock = document.querySelector('#clock');
+  const aw = arrow.getBoundingClientRect().width / 2;
+  const ah = arrow.getBoundingClientRect().height / 2;
+  const clockCenter =
+    clock.getBoundingClientRect().width / 2 +
+    clock.getBoundingClientRect().left;
+  const spacerHeight = 80;
+  const pointerX = clockCenter - aw;
+  const pointerY =
+    clock.getBoundingClientRect().height * 0.41 - ah + spacerHeight;
+  arrow.style.left = pointerX;
+  arrow.style.top = pointerY;
+
   arrow.style.transform = `rotate(${angle}rad)`;
   const buttonRight = document.querySelector('#button_right');
   const buttonLeft = document.querySelector('#button_left');
